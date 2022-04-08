847278f2b900f801499d0cef88ce573754c2574f
sa46lll <9812smh@naver.com>
2022-04-08 15:39:28 +0900

    feat: viewset Comment API 추가

--- api2/urls.py
+++ api2/urls.py
@@ -2,11 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 
-from api2.views import UserViewSet, PostViewSet
+from api2.views import UserViewSet, PostViewSet, CommentViewSet
 
 router = routers.DefaultRouter()
 router.register(r'user', UserViewSet)
 router.register(r'post', PostViewSet)
+router.register(r'comment', CommentViewSet)
+
 
 urlpatterns = [
     path('', include(router.urls)),     # api2/users/
